c2a9e2bcbe0ec0ff34997b5a37b1d7c9f650aa24
Vinod <vinodkzr@gmail.com>
2021-09-08 01:37:29 +0530

    Updated Greeter.jva file by Vinod Gopi

--- DevopsBasic-master/server/src/main/java/com/example/Greeter.java
+++ DevopsBasic-master/server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,10 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public String greet(String someone) 
+  {
     return String.format("Hello How are you, %s!", someone);
+    return String.format("Hello What are you Doing %s!", someone);
+    return String.format("Hello Where ur staying, %s!", someone);
   }
 }
